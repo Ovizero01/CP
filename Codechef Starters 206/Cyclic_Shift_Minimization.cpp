4c0a4c64126225c7142920cf413e0d1124ec9a2a
DESKTOP-EBOTGVS\User <ahmed8050520@gmail.com>
2025-10-01 23:30:14 +0600

    Merge branch 'main' of https://github.com/Ovizero01/Competitive-Programming

---
+++ Codechef Starters 206/Cyclic_Shift_Minimization.cpp
@@ -0,0 +1,21 @@
+//https://www.codechef.com/problems/CYCLICMIN
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve(){
+    int n; cin >> n;
+    for(int i = n; i >= 1; i--) cout << i << " ";
+    cout << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T; cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
